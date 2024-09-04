c8a74369a7fc953e21f4ba75307dd410a18f3805
mobinarohani <mobinarohani4@gmail.com>
2024-09-05 01:22:52 +0330

    Added Experience & works Components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Intro from './Components/Intro/Intro';
 import Services from './Components/Services/Services';
+import Experience from './Components/Experience/Experience';
+import Works from './Components/Works/Works';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Navbar/>
       <Intro/>
       <Services/>
+      <Experience/>
+      <Works/>
     </div>
   );
 }
